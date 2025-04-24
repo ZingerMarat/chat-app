b5f82f79a67b77c00a3e4b74110522aff6d7c6eb
Zinger Marat <zingermarat@gmail.com>
2025-04-24 13:11:15 +0300

    refactor: update server port configuration and connect to database

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,15 +1,18 @@
 import express from "express";
 import dotenv from "dotenv";
+import { connectDB } from "./lib/db.js";
 
 import authRoutes from "./routes/auth.route.js";
 
 dotenv.config();
 
 const app = express();
+const PORT = process.env.PORT;
 
 app.use(express.json());
 app.use("/api/auth", authRoutes);
 
-app.listen(5000, () => {
-    console.log("server is running on PORT:" + 5000);
+app.listen(PORT, () => {
+    console.log("server is running on PORT:" + PORT);
+    connectDB();
   });
