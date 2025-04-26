354a8e8e44939fd62224c1bd886450d32b4a0919
Zinger Marat <zingermarat@gmail.com>
2025-04-26 11:59:42 +0300

    feat: add profile update functionality with Cloudinary integration and protect route middleware

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 import { generateToken } from "../lib/utils.js";
 import User from "../models/user.model.js";
 import bcrypt from "bcryptjs";
+import cloudinary from "../lib/cloudinary.js"
 
 
 export const signup = async (req, res) => {
@@ -92,4 +93,25 @@ export const logout = (req, res) => {
         console.log("Error in logout controller", err.message)
         res.status(500).json({message: "Internal Server Error"})
     }
+}
+
+export const updateProfile = async (req, res) => {
+  try{
+    const {profilePic} = req.body;
+    const userId = req.user._id;
+
+    if(!profilePic){
+      return res.status(400).json({message: "Profile picture is required"})
+    }
+
+    const uploadResponse = await cloudinary.uploader.upload(profilePic)
+    const updatedUser = await User.findByIdAndUpdate(userId, {profilePic: uploadResponse.secure_url}, {new: true})
+
+    res.status(200).json(updatedUser)
+
+
+  }catch (err) {
+    console.log("Error in updateProfile controller", err.message)
+    res.status(500).json({message: "Internal Server Error"})
+  }
 }
