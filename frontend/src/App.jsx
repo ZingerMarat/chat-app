f45cd7a970f121de69620b8e8e1ba8e84a4cfd85
Zinger Marat <zingermarat@gmail.com>
2025-04-27 22:54:36 +0300

    feat: add CORS support and enhance authentication check in frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,15 +6,25 @@ import LoginPage from "./pages/LoginPage";
 import SettingsPage from "./pages/SettingsPage";
 import ProfilePage from "./pages/ProfilePage";
 import { useAuthStore } from "./store/useAuthStore";
+import {useEffect} from "react"
 
 const App = () => {
-  const {authUser, checkAuth} = useAuthStore()
+  const {authUser, checkAuth, isCheckingAuth} = useAuthStore()
 
   useEffect(() => {
     checkAuth()
   }, [checkAuth])
 
   console.log(authUser)
+
+  if(isCheckingAuth && !authUser){
+    return(
+      <div className="flex justify-center items-center h-screen">
+        <Loader className="size-10 animate-spin" />
+      </div>
+    )
+  }
+
   
   return(
     <div>
