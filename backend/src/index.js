d77e8d0bdfb2eeb55ca615416ab24c4282f3cfbe
Zinger Marat <zingermarat@gmail.com>
2025-05-08 13:14:30 +0300

    fix: downgrade express to version 4.18.2 for compatibility; format code for consistency

--- backend/src/index.js
+++ backend/src/index.js
@@ -8,7 +8,7 @@ import path from "path";
 
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
-import {app, server} from "./lib/socket.js";
+import { app, server } from "./lib/socket.js";
 
 dotenv.config();
 
@@ -18,12 +18,12 @@ const __dirname = path.resolve()
 
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors({origin: "http://localhost:5173", credentials: true}));
+app.use(cors({ origin: "http://localhost:5173", credentials: true }));
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes)
 
-if(process.env.NODE_ENV === "production"){
+if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/dist")))
 
   app.get("*", (req, res) => {
@@ -32,6 +32,6 @@ if(process.env.NODE_ENV === "production"){
 }
 
 server.listen(PORT, () => {
-    console.log("server is running on PORT:" + PORT);
-    connectDB();
-  });
+  console.log("server is running on PORT:" + PORT);
+  connectDB();
+});
